7ba125bbe1989a9e1be708a7d610675434e8aed1
midnite_runr <jhpitts@gmail.com>
2022-05-07 14:03:19 -0400

    actual 0.0.2

--- build_stuff.zsh
+++ build_stuff.zsh
@@ -1,1 +1,7 @@
+rm -rf build/
+rm -rf dist/
+rm -rf encryptAgit/src/encryptAgit.egg-info
+
 python3 setup.py sdist bdist_wheel
+
+python3 -m twine upload --verbose dist/*
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="encryptAgit",
-    version="0.0.1",
+    version="0.0.2",
     author="Josh Pitts https://twitter.com/ausernamedjosh",
     author_email="the.midnite.runr@gmail.com",
     description="Encrypt your git repo",

a8784a8d637ec226daf409650c910d38ad3206d8
midnite_runr <jhpitts@gmail.com>
2022-05-12 08:18:14 -0400

    0.1.0: setup change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="encryptAgit",
-    version="0.0.9",
+    version="0.1.0",
     author="Josh Pitts https://twitter.com/ausernamedjosh",
     author_email="the.midnite.runr@gmail.com",
     description="Encrypt your git repo",
